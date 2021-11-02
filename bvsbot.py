a373a3aaab4ccaf94e8fd61aeb14ddab91969e29
tn5421 <2477053+tn5421@users.noreply.github.com>
2021-11-01 22:01:00 -0500

    modified allies.yaml per #5
    minor formatting fixes in ally.py
    
    corrected oversight in allies.yaml
    
    renamed bot, changed readme
    
    fixed NoneType output

--- bvsbot.py
+++ bvsbot.py
@@ -7,8 +7,8 @@ from discord.ext import commands
 data = yaml.load(open("settings.yaml", 'r'))
 token = str(data.get('token'))
 
-client = commands.Bot(command_prefix="!", description="A simple WKAI bot written for "\
-  + "you guys, the community!")
+client = commands.Bot(command_prefix="!", description="A simple WKAI" \
+  + "bot written for you guys, the community!")
 
 for file in os.listdir("modules"):
   if file.endswith(".py"):
--- modules/ally.py
+++ modules/ally.py
@@ -9,35 +9,35 @@ class ally(commands.Cog, name="Allies"):
   def init(self, bot: commands.Bot):
     self.bot = bot
 
-  @commands.command(name='ally', brief='Find info on allies!', help='Find more information '\
-  + 'on allies! Please format your command like this: !ally billy2.')
+  @commands.command(name="ally", brief="Find info on allies!", help="Find" \
+    + "more information on allies! Please format your command like this: " \
+    + "!ally billy2.")
   async def ally(self, ctx: commands.Context, userinput: str):
     
     # allies.yaml will not be categorized in a manner 
     # consistent with the bvs.wikidot.com wiki. 
-    # Instead, feel free to add categorization subtags
-    # to their 'type' subattribute.
-    
+
     userinput = userinput.lower()
     stream = open("data/allies.yaml", "r") # abstracted so we can close it again
     allies = yaml.load(stream)
     
-    if allies.get(userinput) == 0:
+    userQuery = allies.get(userinput)
+    stream.close() # close file.
+    
+    if userQuery is None:
       output = "Does not match an ally in the database! Pester tn5421#0271 if this is in error!"
     else:
-      output = str(allies.get(userinput))
+      output = str(userQuery)
       
-      output=re.sub('\'((?=type\':)|(?=obtain\':)|(?=description\':)|(?=bonus\':)|(?=teams\':)|(?=recommendation\':)|(?=greeting\':))', '  ', output) 
+      output = re.sub('\'((?=type\':)|(?=obtain\':)|(?=description\':)|(?=bonus\':)|(?=teams\':)|(?=recommendation\':)|(?=greeting\':))', '  ', output)
       # remove leading quotation marks on categories
-      output=re.sub('\':', ':', output) # remove trailing quotation marks on categories
-      output=re.sub('{', ' ', output) # replace the opening { with a space
-      output=re.sub('[}\[\]]', '', output) # strip out some undesired characters
-      output=re.sub('\'[^0-9A-Za-z+\-:"]', '\'\n', output) # insert a newline character after closing quotation marks
-      output=re.sub('"[^0-9A-Za-z+\-:"\']', '"\n', output) # catch double quotation marks that were missed by previous line
-      output=re.sub('\n +\'', '\n          \'', output) # align non-category lines
+      output = re.sub('\':', ':', output) # remove trailing quotation marks on categories
+      output = re.sub('{', ' ', output) # replace the opening { with a space
+      output = re.sub('[}\[\]]', '', output) # strip out some undesired characters
+      output = re.sub('\'[^0-9A-Za-z+\-:"]', '\'\n', output) # insert a newline character after closing quotation marks
+      output = re.sub('"[^0-9A-Za-z+\-:"\']', '"\n', output) # catch double quotation marks that were missed by previous line
+      output = re.sub('\n +\'', '\n          \'', output) # align non-category lines
     
-    stream.close() # close file.
-
     await ctx.send(output)
     print("Ally Shenanigans!")
 
